7bce54c07f29b494fa4e8e937641aa77b2938af6
Jimmy Gilces <jimmygilces@gmail.com>
2020-09-29 17:37:47 -0500

    no tomar

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         //Oracle
         Yajra\Oci8\Oci8ServiceProvider::class,
+
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -227,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
